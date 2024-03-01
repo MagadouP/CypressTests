7076b3db9a964930df5514365400e16bc89010a9
MagadouP <152959026+MagadouP@users.noreply.github.com>
2024-03-01 19:47:54 +0100

    pierwszy sukces z dodaniem plikow

--- cypress/e2e/spec.hw02-modul2.cy.js
+++ cypress/e2e/spec.hw02-modul2.cy.js
@@ -3,8 +3,13 @@ describe('HW02_modul2_User can visit GoIT page', () => {
     cy.visit('https://www.edu.goit.global/account/login');
 });
 
-  it('successfully login to the GoIT page', () => {
+  it('successfully login to the GoIT page as user', () => {
     cy.logInUser('user888@gmail.com', '1234567890');
+    cy.get('[id="go-to-the-course-homepage-widget"]').scrollIntoView().should("be.visible");
+  });
+
+  it('successfully login to the GoIT page as admin', () => {
+    cy.logInUser('testowyqa@qa.team', 'QA!automation-1');
     cy.get('[id="go-to-the-course-homepage-widget"]').scrollIntoView().should("be.visible");
   });
 });
