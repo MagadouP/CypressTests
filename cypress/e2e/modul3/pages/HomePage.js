11c7a9ef32d6c93ba7f99e31cd57f215178f3edf
MagadouP <152959026+MagadouP@users.noreply.github.com>
2024-03-02 17:35:39 +0100

    export new HomePage

---
+++ cypress/e2e/modul3/pages/HomePage.js
@@ -0,0 +1,21 @@
+class HomePage {
+    constructor() {
+        this.courseHomepageWidget = '[id="go-to-the-course-homepage-widget"]';
+        this.nextButton = 'button[class="next-7afvtf elphyiqy6"]';
+        this.logOutOption = 'Log out';
+    }
+
+    goToCourseHomepage() {
+        cy.get(this.courseHomepageWidget).scrollIntoView().should("be.visible").wait(5000);
+    }
+
+    proceedToNext() {
+        cy.get(this.nextButton).click().wait(5000);
+    }
+
+    logOut() {
+        cy.get(this.logOutOption).click();
+    }
+}
+
+export default new HomePage();
