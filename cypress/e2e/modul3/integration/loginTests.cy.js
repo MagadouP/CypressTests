f0caee7dc5e81bc02a3fd41c21c313b644e0db0d
MagadouP <152959026+MagadouP@users.noreply.github.com>
2024-03-04 16:16:55 +0100

    loginTests modified

--- cypress/e2e/modul3/integration/loginTests.cy.js
+++ cypress/e2e/modul3/integration/loginTests.cy.js
@@ -8,14 +8,14 @@ describe('HW03_modul3_User can visit GoIT page', () => {
 
     it('successfully login to the GoIT page as user', () => {
         LoginPage.logInUser('user888@gmail.com', '1234567890');
-        HomePage.goToCourseHomepageWidget();
+        HomePage.goToCourseHomepage();
         HomePage.proceedToNext();
         HomePage.logOut();
     });
 
     it('successfully login to the GoIT page as admin', () => {
         LoginPage.logInUser('testowyqa@qa.team', 'QA!automation-1');
-        HomePage.goToCourseHomepageWidget();
+        HomePage.goToCourseHomepage();
         HomePage.proceedToNext();
         HomePage.logOut();
 });
